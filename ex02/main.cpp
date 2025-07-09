28c4f9f7e907ab47cee3e7239bd00190de65abae
yuu-ux <yuugaehara@gmail.com>
2025-07-09 20:04:33 +0900

    :heavy_plus_sign: ex02 追加

---
+++ ex02/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main(void) {
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string& stringREF = *stringPTR;
+	std::cout << &str << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+	std::cout << str << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+}
